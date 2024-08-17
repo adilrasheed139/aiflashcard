4ba9fdfd61ebc7309b83d0e84718ed8099fac143
🔍 Data Science Enthusiast | SWE Fellow @Headstarter AI | ML | R | Power BI & Tableau | WordPress & Learndash Specialist <160167319+adilrasheed139@users.noreply.github.com>
2024-08-17 16:29:10 +0500

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,12 @@ export default defineConfig({
     outDir: 'dist',
     rollupOptions: {
       // Ensure these dependencies are externalized
-      external: ['@mui/material', '@emotion/react', '@emotion/cache'],
+      external: [
+        '@mui/material',
+        '@emotion/react',
+        '@emotion/cache',
+        '@emotion/styled'
+      ],
     },
   },
   server: {
