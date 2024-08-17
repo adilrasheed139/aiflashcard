e74f92107c293607abdc75f3b9eea41897c45c7e
adilrasheed139 <adilrasheed139@gmail.com>
2024-08-17 23:24:53 +0500

    change the openaicontrooler js file code

--- server/controller/openAIController.js
+++ server/controller/openAIController.js
@@ -2,22 +2,31 @@ const { OpenAI } = require('openai');
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 
-const openai = initializeOpenAI();
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-function initializeOpenAI() {
-  try {
-    const apiKey = process.env.OPENAI_API_KEY;
-    if (!apiKey) {
-      throw new Error('OpenAI API key is not set.');
-    }
-    return new OpenAI({
-      apiKey: apiKey,
-    });
-  } catch (error) {
-    console.error('Error initializing OpenAI:', error);
-    return undefined;
-  }
-}
+const API_KEY = "AIzaSyA4cYmr9rV9e5fT3b9bbz2tEWiexklrBtE"
+
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
+
+
+
+
+// const openai = initializeOpenAI();
+
+// function initializeOpenAI() {
+//   try {
+//     const apiKey = process.env.OPENAI_API_KEY;
+//     if (!apiKey) {
+//       throw new Error('OpenAI API key is not set.');
+//     }
+//     return new OpenAI({
+//       apiKey: apiKey,
+//     });
+//   } catch (error) {
+//     console.error('Error initializing OpenAI:', error);
+//     return undefined;
+//   }
+// }
 
 module.exports = {
   async createFlashCards(req, res) {
@@ -27,15 +36,20 @@ module.exports = {
       }
       
       const systemMessage = `You are a teacher who makes flash card decks for students. Return flash cards with JSON parameters (front:string, back:string).`;
-      const userMessage = `Using '${req.body.front}' as a front side card example and '${req.body.back}' as back side card example, create ${req.body.cardCount} flash cards. Ensure all cards fit under the classification of '${req.body.title}'.`;
+      // const userMessage = `Using '${req.body.front}' as a front side card example and '${req.body.back}' as back side card example, create ${req.body.cardCount} flash cards. Ensure all cards fit under the classification of '${req.body.title}'.`;
+      const prompt = `Using '${req.body.front}' as a front side card example and '${req.body.back}' as back side card example, create ${req.body.cardCount} flash cards. Ensure all cards fit under the classification of '${req.body.title}'.`;
 
-      const completion = await openai.chat.completions.create({
-        messages: [{ role: 'system', content: systemMessage }, { role: 'user', content: userMessage }],
-        model: 'gpt-3.5-turbo',
-      });
+      const result = await model.generateContent(systemMessage + prompt);
+      const response = await result.response;
+      const text = response.text();
 
-      const jObj = JSON.parse(completion.choices[0].message.content);
-      return res.status(200).json(jObj);
+      // const completion = await openai.chat.completions.create({
+      //   messages: [{ role: 'system', content: systemMessage }, { role: 'user', content: userMessage }],
+      //   model: 'gpt-3.5-turbo',
+      // });
+
+      // const jObj = JSON.parse(completion.choices[0].message.content);
+      return res.status(200).json(text);
 
     } catch (error) {
       console.error('Error creating flashcards:', error);
