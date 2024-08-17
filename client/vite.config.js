676a12dc4d6b77a82f81dc1d2108ee6eef623142
🔍 Data Science Enthusiast | SWE Fellow @Headstarter AI | ML | R | Power BI & Tableau | WordPress & Learndash Specialist <160167319+adilrasheed139@users.noreply.github.com>
2024-08-17 15:55:01 +0500

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,24 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: './dist', // Ensures build output goes to the correct folder
+  },
   server: {
-    port:3000,
+    port: 3000,
     open: true,
     proxy: {
       '/graphql': {
         target: 'http://localhost:3001',
         secure: false,
         changeOrigin: true,
-        
-      },   
-      '/api/openai/createFlashCards' :{
+      },
+      '/api/openai/createFlashCards': {
         target: 'http://localhost:3001',
         secure: false,
         changeOrigin: true,
       },
-    }
-  }
+    },
+  },
 })
