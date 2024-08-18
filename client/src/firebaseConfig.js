bb22dcd8dadc1325e2835c11edef7a676ac4404b
adilrasheed139 <adilrasheed139@gmail.com>
2024-08-18 23:19:47 +0500

    modify the firebaseconfig file

--- client/src/firebaseConfig.js
+++ client/src/firebaseConfig.js
@@ -7,14 +7,14 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBImOpZxGMGSQmVApTLSQ_A38UL5NflcXE",
-    authDomain: "aiflashcard-20bf7.firebaseapp.com",
-    projectId: "aiflashcard-20bf7",
-    storageBucket: "aiflashcard-20bf7.appspot.com",
-    messagingSenderId: "612353817715",
-    appId: "1:612353817715:web:a020bdec7803c2a8753e06",
-    measurementId: "G-QJ608E7KY7"
-  };
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
