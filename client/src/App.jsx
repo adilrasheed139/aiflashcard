9e1cef4baa99277f38de52762405e7e9cc793950
adilrasheed139 <adilrasheed139@gmail.com>
2024-08-19 00:22:05 +0500

    update the appjsx file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-import Navbar from './components/NavBar';
-import LoginPage from './pages/LoginPage'; // Import your LoginPage component
-import DashboardPage from './pages/DashboardPage'; // Import your DashboardPage component or others
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import DashboardPage from './pages/DashboardPage';
+import Navbar from './components/Navbar';
+import { ApolloProvider, InMemoryCache, createHttpLink, setContext } from '@apollo/client';
 
 const httpLink = createHttpLink({
   uri: 'https://aiflashcard-production.up.railway.app/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
   const token = localStorage.getItem('id_token');
-  // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
@@ -35,7 +32,6 @@ function App() {
         <Routes>
           <Route path="/" element={<LoginPage />} />
           <Route path="/dashboard" element={<DashboardPage />} />
-          {/* Add other routes here */}
         </Routes>
       </Router>
     </ApolloProvider>
